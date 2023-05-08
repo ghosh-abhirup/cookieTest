2dbfdf7e1cd5e944a53ca567c4a347b1436dd324
singleufo <ghoshabhirup2001@gmail.com>
2023-05-08 16:46:33 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import useCookies from 'react-cookie/cjs/useCookies'
+import './App.css'
+
+function App() {
+  const [cookie, setCookie] = useCookies(['data'])
+  const dataValue = {
+    company: "Perch"
+  }
+  useEffect(()=>{
+    setCookie('data', dataValue, [{ path:'/'}, {domain:'.netlify.app'}])
+  },[])
+
+  console.log(cookie['data'])
+
+  return (
+    <>
+      <p>Source subdomain</p>
+    </>
+  )
+}
+
+export default App
